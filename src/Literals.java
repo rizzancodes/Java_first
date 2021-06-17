1eaf2b6b7e06d1b56b8420d3cd56559b6a4fb237
Rizzan <rizzanzaveri@gmail.com>
2021-06-17 13:20:10 +0500

    Branch2 commit

---
+++ src/Literals.java
@@ -0,0 +1,23 @@
+
+public class Literals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte age = 21;
+		char ch = 'R';
+		float pi = 3.14f;
+		double val = 4.546d;
+		long bigValue = 56666687771654334L;
+		short shortVal = 56;
+		int intVal = 79;
+		
+		
+		String name = "Rizzan";
+	
+		
+		System.out.println(val+pi+ch+21);
+
+	}
+
+}
